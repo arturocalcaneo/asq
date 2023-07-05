8cedcce6235302cd2c5d4352ba95f57f470feb2c
Arturo Alejandro Calcaneo Badillo <aa.calcaneob@gmail.com>
2023-07-04 20:28:58 -0600

    Avance 4 - Proyecto ASQ

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,18 +42,18 @@ $(document).ready(function(){
 
 	  // Navigation arrows
 	  navigation: {
-	    nextEl: '.swiper.second' + ' > .swiper-button-next',
-	    prevEl: '.swiper.second' + ' > .swiper-button-prev',
+	    nextEl: '.swiper.first' + ' > .swiper-button-next',
+	    prevEl: '.swiper.first' + ' > .swiper-button-prev',
 	  }
 	});
 
 	/**
 	 * Importar Sección:
-	 *  -> Acerca de
+	 *  -> Organizadores (1)
 	 * */
 	$('#organizadores').load(raizSecciones + '/organizadores.seccion.html');
 
-	// ACTIVAR EL PRIMER SILDER DE IMÁGENES
+	// ACTIVAR EL SEGUNDO SILDER DE IMÁGENES
 	new Swiper('.swiper.second', {
 	  direction: 'horizontal',
 	  loop: true,
@@ -82,4 +82,59 @@ $(document).ready(function(){
 	  }
 	});
 
+	/**
+	 * Importar Sección:
+	 *  -> Organizadores (2)
+	 * */
+	$('#organizadores-2').load(raizSecciones + '/organizadores2.seccion.html');
+
+	// ACTIVAR EL TERCER SILDER DE IMÁGENES
+	new Swiper('.swiper.third', {
+	  direction: 'horizontal',
+	  loop: true,
+
+	  // Navigation arrows
+	  navigation: {
+	    nextEl: '.swiper.third' + ' > .swiper-button-next',
+	    prevEl: '.swiper.third' + ' > .swiper-button-prev',
+	  },
+	  slidesPerView: 5,
+	  spaceBetween: 15,
+
+	  breakpoints: {
+	  	1152: {
+	  		slidesPerView: 5
+	  	},
+	  	1024: {
+	  		slidesPerView: 4
+	  	},
+	  	512: {
+	  		slidesPerView: 2
+	  	},
+	  	128: {
+	  		slidesPerView: 1
+	  	}
+	  }
+	});
+
+	/**
+	 * Importar Sección:
+	 *  -> Asesores Facultativos
+	 * */
+
+	$('#asesores-facult').load( raizSecciones + '/asesoresfacultativos.seccion.html' );
+
+	/**
+	 * Importar Sección:
+	 *  -> Conteos
+	 * */
+
+	$('#contador').load( raizSecciones + '/conteos.seccion.html' );
+
+	/**
+	 * Importar Sección:
+	 *  -> Patrocinadores
+	 * */
+
+	$('#patrocinadores').load( raizSecciones + '/patrocinadores.seccion.html' );
 });
